f0103b5c377c7330f4a069109bb03f1244dd1c9b
Maxio Mugisha <maximugisha@gmail.com>
2024-12-21 08:30:07 +0300

    fixed some issues, now major processes done

--- app/rate-institution/page.tsx
+++ app/rate-institution/page.tsx
@@ -170,6 +170,7 @@ export default function CreateInstitutionPage() {
                 type="button"
                 onClick={() => setShowNewInstitution(true)}
                 className="text-sm text-blue-600 hover:text-blue-700 flex items-center gap-1 ml-4"
+                disabled
               >
                 <PlusCircle className="w-4 h-4" />
                 Add New
